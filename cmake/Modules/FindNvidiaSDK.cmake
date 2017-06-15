5e1b1b12f8f60ed1bd3ba5a2f7c31156c7981e40
Brandon Haynes <bhaynes@cs.washington.edu>
2017-06-15 14:18:21 -0700

    CMake tweaks, add initial fixture for GPU tests

--- cmake/Modules/FindNvidiaSDK.cmake
+++ cmake/Modules/FindNvidiaSDK.cmake
@@ -1,20 +1,27 @@
 # Finds the nvidia-encode library. This module defines:
 #   - NVIDIASDK_INCLUDE_DIR, include file path
+#   - NVIDIASDK_SOURCE_DIR, dynamic link source path
 #   - NVIDIASDK_FOUND, whether the Nvidia SDK has been found
 
 if(NVIDIASDK_SEARCH_HEADER_PATHS)
   find_path(
       NVIDIASDK_INCLUDE_DIR dynlink_cuda.h
       PATHS ${NVIDIASDK_SEARCH_HEADER_PATHS}
-      NO_DEFAULT_PATH
-  )
+      NO_DEFAULT_PATH)
+  find_path(
+      NVIDIASDK_SOURCE_DIR dynlink_cuda.cpp
+      PATHS ${NVIDIASDK_SEARCH_HEADER_PATHS}
+      NO_DEFAULT_PATH)
 else()
   find_path(NVIDIASDK_INCLUDE_DIR dynlink_cuda.h
-            PATHS file(GLOB /opt/nvidia/Video_Codec_SDK_8.0.14/Samples/common/inc))
+            PATHS file(GLOB /opt/nvidia/Video_Codec_SDK_7.1.9/Samples/common/inc))
+  find_path(NVIDIASDK_SOURCE_DIR dynlink_cuda.cpp
+            PATHS file(GLOB /opt/nvidia/Video_Codec_SDK_7.1.9/Samples/common/src))
 endif()
 
 if(NVIDIASDK_INCLUDE_DIR)
   message(STATUS "Found nvidia-sdk: ${NVIDIASDK_INCLUDE_DIR}")
+  set(NVIDIASDK_SOURCES "${NVIDIASDK_SOURCE_DIR}/dynlink_cuda.cpp;${NVIDIASDK_SOURCE_DIR}/dynlink_nvcuvid.cpp;${NVIDIASDK_SOURCE_DIR}/NvHWEncoder.cpp")
   set(NVIDIASDK_FOUND TRUE)
 else()
   set(NVIDIASDK_FOUND FALSE)
--- test/src/video/test.cc
+++ test/src/video/test.cc
@@ -1,3 +1,33 @@
-void foo()
+#include <gtest/gtest.h>
+#include "dynlink_cuda.h"
+#include "dynlink_nvcuvid.h"
+
+class FrameQueueTestFixture : public testing::Test
 {
+public:
+        virtual void SetUp()
+	{
+	ASSERT_EQ(cuInit(0, __CUDA_API_VERSION, nullptr), CUDA_SUCCESS);
+	ASSERT_EQ(cuvidInit(0), CUDA_SUCCESS);
+	ASSERT_EQ(cuDeviceGet(&device, 0), CUDA_SUCCESS);
+	ASSERT_EQ(cuCtxCreate(&context, CU_CTX_SCHED_AUTO, device), CUDA_SUCCESS);
+	ASSERT_EQ(cuCtxPopCurrent(&context), CUDA_SUCCESS);
+	ASSERT_EQ(cuvidCtxLockCreate(&lock, context), CUDA_SUCCESS);
+	}
+
+        virtual void TearDown()
+	{
+	ASSERT_EQ(cuvidCtxLockDestroy(lock), CUDA_SUCCESS);
+	ASSERT_EQ(cuCtxDestroy(context), CUDA_SUCCESS);
+	}
+
+private:
+	CUdevice device;
+	CUcontext context;
+	CUvideoctxlock lock;
+};
+
+TEST_F(FrameQueueTestFixture, test_RLE)
+{
+
 }
