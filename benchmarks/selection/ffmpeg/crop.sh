2eb1e870d90d847d55d2b6f5618d19e62461473d
Brandon Haynes <bhaynes@cs.washington.edu>
2017-11-02 09:30:07 -0700

    Checked in Ffmpeg scripts

--- benchmarks/selection/ffmpeg/crop.sh
+++ benchmarks/selection/ffmpeg/crop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -hide_banner -y -loglevel error  -i $1 -filter:v "crop=$2:$3:$4:$5" $6
+ffmpeg -hide_banner -y -loglevel error -vcodec h264_cuvid -i $1 -filter:v "crop=$2:$3:$4:$5" -vcodec hevc_nvenc $6
