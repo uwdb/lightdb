bddc214459fec467411055e54e4d3e734f58749c
Brandon Haynes <bhaynes@cs.washington.edu>
2017-12-10 21:00:15 -0800

    Benchmarks

--- benchmarks/partition/opencv/main.cc
+++ benchmarks/partition/opencv/main.cc
@@ -43,11 +43,49 @@ void tile(const char* input_filename,
     }
 }
 
+void time_partition(const char* input_filename, const float delta_time)
+{
+    VideoCapture input(input_filename);
+    auto frames = std::lround(input.get(CAP_PROP_FRAME_COUNT));
+    auto fourcc = std::lround(input.get(CV_CAP_PROP_FOURCC));
+    auto fps = std::lround(input.get(CAP_PROP_FPS));
+    Mat frame;
+    Size size{input.get(CAP_PROP_FRAME_WIDTH),
+              input.get(CAP_PROP_FRAME_HEIGHT)};
+    auto index = 0u;
+    VideoWriter writer{TILE_FILENAME(index++), fourcc, fps, size, true};
+    auto delta_frames = delta_time * fps;
+
+    if(!input.isOpened())
+        throw std::runtime_error("Failed to open input.");
+
+    printf("Partitioning into %.02f second chunks\n", delta_time);
+
+    while(frames--) {
+        input >> frame; // get a new frame from camera
+
+        if(!frame.empty())
+		{
+		writer.write(frame);
+                if(delta_frames-- == 0)
+			{
+			writer.release();
+			writer = VideoWriter{TILE_FILENAME(index++), fourcc, fps, size, true};
+			delta_frames = delta_time * fps;
+			}
+                }
+    }
+}
+
 int main(int argc, char** argv) {
-    if(argc != 4)
-        printf("Usage: %s [input filename] [rows] [columns]\n", argv[0]);
-    else {
-        auto rows = atoi(argv[2]), columns = atoi(argv[3]);
-        tile(argv[1], rows, columns);
+    if(argc != 5 && argc != 4)
+        printf("Usage: %s [tile,time] [input filename] [rows] [columns]\n", argv[0]);
+    else if(std::string(argv[1]) == "tile") {
+        auto rows = atoi(argv[3]), columns = atoi(argv[4]);
+        tile(argv[2], rows, columns);
+    }
+    else if(std::string(argv[1]) == "time") {
+        auto delta_time = atof(argv[3]);
+        time_partition(argv[2], delta_time);
     }
 }
--- benchmarks/partition/opencv/run.sh
+++ benchmarks/partition/opencv/run.sh
@@ -6,12 +6,21 @@ DATASET_EXTENSION=h264
 echo Dataset: $DATASET_NAME
 
 echo "----------------"
+echo "4K Input, 1.5s time partition"
+
+file=$DATASET_PATH${DATASET_NAME}4K.$DATASET_EXTENSION
+time=1.5
+
+ffmpeg -hide_banner -loglevel error -y -i $file -c copy input.mp4
+time ./opencv_partitioner time input.mp4 $time
+
+echo "----------------"
 echo "4K Input, theta partition"
 
 file=$DATASET_PATH${DATASET_NAME}4K.$DATASET_EXTENSION
 
 ffmpeg -hide_banner -loglevel error -y -i $file -c copy input.mp4
-time ./opencv_partitioner input.mp4 1 4
+time ./opencv_partitioner tile input.mp4 1 4
 
 echo "----------------"
 echo "4K Input, phi partition"
@@ -19,7 +28,7 @@ echo "4K Input, phi partition"
 file=$DATASET_PATH${DATASET_NAME}4K.$DATASET_EXTENSION
 
 ffmpeg -hide_banner -loglevel error -y -i $file -c copy input.mp4
-time ./opencv_partitioner input.mp4 4 1
+time ./opencv_partitioner tile input.mp4 4 1
 
 rm input.mp4
 rm tile*.mp4
