6c9058ec0e7bdeb8a08e0299751a8756db24b439
Brandon Haynes <bhaynes@cs.washington.edu>
2017-12-11 21:41:14 -0800

    YOLO WIP

--- core/model/src/Functor.cc
+++ core/model/src/Functor.cc
@@ -67,6 +67,68 @@ namespace visualcloud {
         };
     };
 
+    ObjectDetect::ObjectDetect()
+        : network_(load_network("/home/bhaynes/projects/darknet/cfg/tiny-yolo.cfg", "/home/bhaynes/projects/darknet/tiny-yolo.weights", 0)),
+          metadata_(get_metadata("/home/bhaynes/projects/darknet/cfg/coco.data"))
+    { }
+
+    ObjectDetect::operator const FrameTransform() const {
+        CUresult result;
+        auto threshold = 0.5f, hier_thresh=0.5f, nms=.45f;
+        auto num = num_boxes(network_);
+        //auto im = load_image("/home/bhaynes/projects/darknet/data/dog.jpg", 0, 0, 3);
+        auto im = load_image("/home/bhaynes/projects/darknet/data/dog416.jpg", 416, 416, 3);
+        auto *boxes = make_boxes(network_);
+        auto **probs = make_probs(network_);
+
+        CUdeviceptr output;
+        result = cuMemAlloc(&output, 416 * 416 * sizeof(float));
+        if(result != CUDA_SUCCESS) printf("result %d\n", result);
+
+        if(module_ == nullptr && (result = cuModuleLoad(&module_, "/home/bhaynes/projects/visualcloud/core/model/src/kernels.cubin")) != CUDA_SUCCESS)
+            throw new std::runtime_error(std::string("Failure loading module") + std::to_string(result));
+        else if(function_ == nullptr && (result = cuModuleGetFunction(&function_, module_, "resize")) != CUDA_SUCCESS)
+            throw new std::runtime_error(std::string("Failure loading kernel") + std::to_string(result));
+
+        //result = cuMemFree(output);
+
+        return [=](VideoLock &lock, Frame& frame) mutable -> Frame& {
+            static unsigned int id = 0;
+
+            std::scoped_lock{lock};
+
+            if(id++ % 15 == 0)
+            {
+
+            dim3 blockDims(512,1,1);
+            dim3 gridDims((unsigned int) std::ceil((double)(frame.width() * frame.height() * 3 / blockDims.x)), 1, 1 );
+            CUdeviceptr input = frame.handle(); //, output = frame.handle();
+            auto height = frame.height(), width = frame.width();
+            unsigned int output_height = 416, output_width = 416;
+            float fx = 416.0f/width, fy = 416.0f/height;
+            void *arguments[8] = {&input, &output, &height, &width, &output_width, &output_height, &fx, &fy};
+
+            result = cuLaunchKernel(function_, gridDims.x, gridDims.y, gridDims.z, blockDims.x, blockDims.y, blockDims.z,
+                                   0, nullptr, arguments, nullptr);
+            if(result != CUDA_SUCCESS) printf("result %d\n", result);
+
+            float hostframe[416*416];
+            result = cuMemcpyDtoH(hostframe, output, 416*416*sizeof(float));
+            if(result != CUDA_SUCCESS) printf("result %d\n", result);
+            image hostimage{416, 416, 1, hostframe};
+
+                network_detect(network_, hostimage, threshold, hier_thresh, nms, boxes, probs);
+
+                for(auto i = 0u; i < num; i++)
+                    for(auto j = 0u; j < metadata_.classes; j++)
+                        if(probs[i][j] > 0.001)
+                            printf("%s (%d): %0.2f\n", metadata_.names[j], i, probs[i][j]);
+            }
+
+            return frame;
+        };
+    };
+
     Left::operator const NaryFrameTransform() const {
         return [](VideoLock&, const std::vector<Frame>& frames) -> const Frame& {
             return frames[0];
--- core/model/src/kernels.cu
+++ core/model/src/kernels.cu
@@ -40,3 +40,23 @@ void overlay(unsigned char* left, unsigned char* right, unsigned char* output,
     if(right[index] != transparent_color)
         left[index] = right[index];
 }
+
+extern "C"
+__global__
+void resize(unsigned char* input, unsigned char* output,
+            const unsigned int input_width, const unsigned int input_height,
+            const unsigned int output_width, const unsigned int output_height,
+            const float fx, const float fy) {
+    const unsigned int output_x = blockDim.x * blockIdx.x + threadIdx.x;
+    const unsigned int output_y = blockDim.y * blockIdx.y + threadIdx.y;
+    const unsigned int input_index = output_y * input_width + output_x;
+
+    if (output_x < output_width && output_y < output_height)
+    {
+        const float src_x = output_x * fx;
+        const float src_y = output_y * fy;
+        const unsigned int output_index = src_y * output_width + src_x;
+
+        output[output_index] = input[input_index];
+    }
+}
