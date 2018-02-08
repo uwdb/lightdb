dfb7d52018df32009fb569dccf617d6176e732d3
Brandon Haynes <bhaynes@cs.washington.edu>
2018-02-08 11:38:25 -0800

    Remove unused file

--- test.py
+++
@@ -1,8 +0,0 @@
-from dashtranscoder import DashTranscoder
-
-print 'Tiler: qqq'
-print len(DashTranscoder().tile('foo', 3, 4).tobytes())
-
-
-#print 'Transcoder:'
-#print len(DashTranscoder().transcode('foo').tobytes())
