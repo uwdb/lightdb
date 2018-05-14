7509bdfa6d66d54de5bb2f501573b1e74000017c
Brandon Haynes <bhaynes@cs.washington.edu>
2018-05-14 13:32:30 -0700

    Added stub for homomorphic union operator

---
+++ core/physical/include/Homomorphisms.h
@@ -0,0 +1,56 @@
+#ifndef LIGHTDB_HOMOMORPHISMS_H
+#define LIGHTDB_HOMOMORPHISMS_H
+
+#include "Physical.h"
+
+namespace lightdb::physical {
+
+class HomomorphicUniformTile: public PhysicalLightField, public EncodedVideoOperator {
+public:
+    explicit HomomorphicUniformTile(const LightFieldReference &logical,
+                                    const std::vector<PhysicalLightFieldReference> &parents,
+                                    const unsigned int rows, const unsigned int columns)
+            : PhysicalLightField(logical, parents, DeviceType::CPU),
+              rows_(rows), columns_(columns),
+              sources_(functional::transform<std::reference_wrapper<const EncodedVideoOperator>>(
+                      parents.begin(), parents.end(), [](const PhysicalLightFieldReference &parent) {
+                          return std::ref(parent.downcast<EncodedVideoOperator>()); })),
+              configuration_{get_configuration(sources_, rows_, columns_)}
+    {
+        //TODO assert preconditions
+        throw NotImplementedError();
+    }
+
+    std::optional<physical::DataReference> read() override {
+        //auto chunks = functional::transform(sources_.begin(), sources_.end(), [](auto &source) {
+        //                                        return source.read(); });
+        throw NotImplementedError();
+    }
+
+    const Codec &codec() const override { return Codec::hevc(); }
+    const Configuration &configuration() const override { return configuration_; }
+
+private:
+    const unsigned int rows_, columns_;
+    const std::vector<std::reference_wrapper<const EncodedVideoOperator>> sources_;
+    const Configuration configuration_;
+
+    static const Configuration get_configuration(
+            const std::vector<std::reference_wrapper<const EncodedVideoOperator>>& sources,
+            const unsigned int rows, const unsigned int columns) {
+        auto &first_configuration = sources[0].get().configuration();
+
+        //TODO assert sources[1...n] preconditions (e.g., have same width, height, framerate)
+
+        return Configuration{first_configuration.width * columns,
+                             first_configuration.height * rows,
+                             first_configuration.max_width * columns,
+                             first_configuration.max_height * rows,
+                             0,
+                             first_configuration.framerate};
+    }
+};
+
+} // namespace lightdb::physical
+
+#endif //LIGHTDB_HOMOMORPHISMS_H
