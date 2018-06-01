a7b77e22b3c6c4b8674f4321c1a5025930862288
Armaan Sood <armaan1@cs.washington.edu>
2018-06-01 10:06:39 -0700

    bugs

--- getFFMpegTimes.sh
+++ getFFMpegTimes.sh
@@ -1,13 +1,13 @@
 for var in "$@"
 do
-    T="$(date +%s%M)"
+    T="$(date +%s%3N)"
     ffmpeg -y -i "$var" -c libx264 output.h264 &>/dev/null
-    T="$(($(date +%s%M)-T))"
+    T="$(($(date +%s%3N)-T))"
     echo "$var CPU time: ${T}"
-    T="$(date +%s%M)"
+    T="$(date +%s%3N)"
     ffmpeg -y -i "$var" -c nvenc_h264 output.h264 &>/dev/null
-    T="$(($(date + %s%M)-T))"
-    echo "$var CPU time: ${T}"
+    T="$(($(date +%s%3N)-T))"
+    echo "$var GPU time: ${T}"
 done
 
     
