8a0700f15e079fbd6283dc2cc1620291784c7280
Brandon Haynes <bhaynes@cs.washington.edu>
2018-08-23 15:18:01 -0700

    Incorporate interpolation support into new codebase, add rules for linear->discrete(theta)->discrete(phi).

--- test/src/planning/VisitorTests.cc
+++ test/src/planning/VisitorTests.cc
@@ -35,13 +35,34 @@ TEST_F(VisitorTestFixture, testFoo) {
 
     auto name = "red10"; //std::getenv("TLFNAME");
     auto input = Scan(name);
-//    auto continuous = input.Interpolate(Dimension::Theta, interpolation::NearestNeighbor());
-    auto smalltheta = input.Discretize(Dimension::Theta, rational_times_real({2, 160}, PI));
-    auto small = smalltheta.Discretize(Dimension::Phi, rational_times_real({2, 160}, PI));
+    auto continuous = input.Interpolate(Dimension::Theta, interpolation::Linear());
+    auto smalltheta = continuous.Discretize(Dimension::Theta, rational_times_real({2, 416}, PI));
+    auto small = smalltheta.Discretize(Dimension::Phi, rational_times_real({1, 416}, PI));
     auto gray = small.Map(yolo);
     //auto gray = input.Map(lightdb::Greyscale);
     //auto gray = input.Map(yolo);
     auto encoded = gray.Encode(Codec::boxes());
+
+    auto environment = LocalEnvironment();
+    auto coordinator = Coordinator();
+    Plan plan = HeuristicOptimizer(environment).optimize(encoded);
+
+    print_plan(plan);
+
+    coordinator.save(plan, "dout.hevc");
+}
+
+TEST_F(VisitorTestFixture, testInterpolateDiscretizeMap) {
+    auto yolo = lightdb::extensibility::Load("yolo");
+
+    auto name = "red10";
+    auto input = Scan(name);
+    auto continuous_t = input.Interpolate(Dimension::Theta, interpolation::Linear());
+    auto continuous = continuous_t.Interpolate(Dimension::Phi, interpolation::Linear());
+    auto discrete_t = continuous.Discretize(Dimension::Theta, rational_times_real({2, 416}, PI));
+    auto downsampled = discrete_t.Discretize(Dimension::Phi, rational_times_real({1, 416}, PI));
+    auto boxes = downsampled.Map(yolo);
+    auto encoded = boxes.Encode(Codec::boxes());
 
     auto environment = LocalEnvironment();
     auto coordinator = Coordinator();
