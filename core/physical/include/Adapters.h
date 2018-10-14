edc1811745b8667ae452d4db28f67ed7dcb8b5a0
Brandon Haynes <bhaynes@cs.washington.edu>
2018-10-14 11:44:06 -0700

    Move teed state into tees to ensure state survives tee-er

--- core/physical/include/Adapters.h
+++ core/physical/include/Adapters.h
@@ -39,6 +39,7 @@ private:
  * This is necessary while there are separate hierarchies for CPU and GPU operators,
  * which is sad and should be fixed.
  */
+//TODO Fix the sadness
 class GPUOperatorAdapter: public GPUOperator {
 public:
     explicit GPUOperatorAdapter(const PhysicalLightFieldReference &source)
@@ -85,14 +86,14 @@ static std::shared_ptr<TeedPhysicalLightFieldAdapter> make(const PhysicalLightFi
 }
 
 TeedPhysicalLightFieldAdapter(const PhysicalLightFieldReference &source,
-                              const size_t size)
-        : mutex_(std::make_shared<std::mutex>()),
-          source_(source),
-          queues_{size} {
+                              const size_t size) {
+    auto mutex = std::make_shared<std::mutex>();
+    auto queues = std::make_shared<std::vector<std::queue<MaterializedLightFieldReference>>>(size);
+
     for(auto index = 0u; index < size; index++)
         tees_.emplace_back(
                 LightFieldReference::make<PhysicalToLogicalLightFieldAdapter>(
-                        PhysicalLightFieldReference::make<TeedPhysicalLightField>(*this, index)));
+                        PhysicalLightFieldReference::make<TeedPhysicalLightField>(mutex, queues, source, index)));
     }
 
     TeedPhysicalLightFieldAdapter(const TeedPhysicalLightFieldAdapter&) = delete;
@@ -106,54 +107,57 @@ TeedPhysicalLightFieldAdapter(const PhysicalLightFieldReference &source,
         return tees_.at(index);
     }
 
+    PhysicalLightFieldReference physical(const size_t index) {
+        return (*this)[index].downcast<PhysicalToLogicalLightFieldAdapter>().source();
+    }
+
 private:
     class TeedPhysicalLightField: public PhysicalLightField {
     public:
-        TeedPhysicalLightField(TeedPhysicalLightFieldAdapter &adapter,
+        TeedPhysicalLightField(std::shared_ptr<std::mutex> mutex,
+                               std::shared_ptr<std::vector<std::queue<MaterializedLightFieldReference>>> queues,
+                               const PhysicalLightFieldReference &source,
                                const size_t index)
-                : PhysicalLightField(adapter.source_->logical(),
-                                     {adapter.source_},
-                                     adapter.source_->device()),
-                  adapter_(adapter),
-                  index_(index)
+                : PhysicalLightField(source->logical(),
+                                     {source},
+                                     source->device()),
+                  index_(index),
+                  mutex_(std::move(mutex)),
+                  source_(source),
+                  queues_(std::move(queues))
         { }
 
         TeedPhysicalLightField(const TeedPhysicalLightField&) = delete;
         TeedPhysicalLightField(TeedPhysicalLightField&&) = default;
 
         std::optional<physical::MaterializedLightFieldReference> read() override {
-            return adapter_.read(index_);
+            std::optional<physical::MaterializedLightFieldReference> value;
+
+            std::lock_guard lock(*mutex_);
+
+            auto &queue = queues_->at(index_);
+
+            if(queue.empty())
+                // Teed stream's queue is empty, so read from base stream and broadcast to all queues
+                if((value = source_->read()).has_value())
+                    std::for_each(queues_->begin(), queues_->end(), [&value](auto &q) { q.push(value.value()); });
+
+            // Now return a value (if any) from this tee's queue
+            if(!queue.empty()) {
+                value = queue.front();
+                queue.pop();
+                return value;
+            } else
+                return {};
         }
 
     private:
-        TeedPhysicalLightFieldAdapter &adapter_;
         const size_t index_;
+        std::shared_ptr<std::mutex> mutex_;
+        PhysicalLightFieldReference source_;
+        std::shared_ptr<std::vector<std::queue<MaterializedLightFieldReference>>> queues_;
     };
 
-    std::optional<physical::MaterializedLightFieldReference> read(const size_t index) {
-        std::optional<physical::MaterializedLightFieldReference> value;
-
-        std::lock_guard lock(*mutex_);
-
-        auto &queue = queues_[index];
-
-        if(queue.empty())
-            // Teed stream's queue is empty, so read from base stream and broadcast to all queues
-            if((value = source_->read()).has_value())
-                std::for_each(queues_.begin(), queues_.end(), [&value](auto &q) { q.push(value.value()); });
-
-        // Now return a value (if any) from this tee's queue
-        if(!queue.empty()) {
-            value = queue.front();
-            queue.pop();
-            return value;
-        } else
-            return {};
-    }
-
-    std::shared_ptr<std::mutex> mutex_;
-    PhysicalLightFieldReference source_;
-    std::vector<std::queue<MaterializedLightFieldReference>> queues_;
     std::vector<LightFieldReference> tees_;
 };
 
