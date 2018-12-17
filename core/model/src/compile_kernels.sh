be5c23c8fd41399d1afeab3ec45f993b0d0b5592
Brandon Haynes <bhaynes@cs.washington.edu>
2018-12-17 13:11:44 -0800

    No longer need legacy kernel logic

--- core/model/src/compile_kernels.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#sudo update-alternatives --set gcc /usr/bin/gcc-4.9
-nvcc -ccbin /usr/bin/gcc-4.9 -arch=compute_60 -code=sm_60 -cubin /home/bhaynes/projects/visualcloud/core/model/src/kernels.cu -odir /home/bhaynes/projects/visualcloud/core/model/src
-#sudo update-alternatives --auto gcc
