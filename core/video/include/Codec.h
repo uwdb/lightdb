05568dfe7958078970fa765b9971b8135a0321f6
Brandon Haynes <bhaynes@cs.washington.edu>
2019-03-04 11:41:41 -0800

    Add canonical file extension to codec

--- core/video/include/Codec.h
+++ core/video/include/Codec.h
@@ -2,6 +2,7 @@
 #define LIGHTDB_CODEC_H
 
 #include "EncodeAPI.h"
+#include <vector>
 
 namespace lightdb {
 
@@ -27,11 +28,25 @@ namespace lightdb {
             return value;
         }
 
+        static const std::vector<Codec>& all() {
+            static const std::vector<Codec> codecs{raw(), h264(), hevc(), boxes()};
+            return codecs;
+        }
+
+        static const std::optional<Codec> get(const std::string &name) {
+            auto codec = std::find_if(Codec::all().begin(), Codec::all().end(),
+                                      [&name](const auto &c) { return strncasecmp(name.c_str(), c.name().c_str(), name.length()) == 0 ||
+                                                                      strncasecmp(name.c_str(), c.extension().c_str(), name.length()) == 0; });
+            return codec != Codec::all().end()
+                ? std::optional<Codec>{*codec} : std::nullopt;
+        }
+
         Codec(Codec&&) = default;
         Codec(const Codec&) = default;
 
         const std::string& name() const { return name_; }
         const std::string& fourcc() const { return fourcc_; }
+        const std::string& extension() const { return extension_; }
         const std::optional<EncodeCodec>& nvidiaId() const { return nvidiaId_; }
         const std::optional<cudaVideoCodec>& cudaId() const { return cudaId_; }
 
@@ -49,11 +64,13 @@ namespace lightdb {
     private:
         explicit Codec(std::string name, std::string fourcc,
                 std::optional<EncodeCodec> nvidiaId={}, std::optional<cudaVideoCodec> cudaId={}) noexcept
-                : name_(std::move(name)), fourcc_(std::move(fourcc)), nvidiaId_{nvidiaId}, cudaId_{cudaId}
+                : name_(std::move(name)), fourcc_(std::move(fourcc)), extension_('.' + name_),
+                  nvidiaId_{nvidiaId}, cudaId_{cudaId}
         { CHECK_EQ(fourcc_.size(), 4); }
 
         const std::string name_;
         const std::string fourcc_;
+        const std::string extension_;
         const std::optional<EncodeCodec> nvidiaId_;
         const std::optional<cudaVideoCodec> cudaId_;
     };
