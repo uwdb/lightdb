544d99a432ec948f328575161fea65657a12ce8b
Brandon Haynes <bhaynes@cs.washington.edu>
2019-03-20 14:37:31 -0700

    Travis

--- cmake/nvenc.cmake
+++ cmake/nvenc.cmake
@@ -1,8 +1,5 @@
-set(NVENC_ROOT thirdparty/nv-codec-headers)
+set(NVENC_ROOT thirdparty/NvEnc)
 
 include_directories(SYSTEM
-    ${PROJECT_SOURCE_DIR}/${NVENC_ROOT}
     ${PROJECT_SOURCE_DIR}/${NVENC_ROOT}/inc
     )
-
-#add_library(nvenc ${NVENC_SOURCES})
