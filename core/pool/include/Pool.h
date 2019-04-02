c58176b253f6cf0c62f1a7c2cb6ec4dc7e9d15e4
Brandon Haynes <bhaynes@cs.washington.edu>
2019-04-01 17:46:55 -0700

    unordered_map is not thread safe, so guard it with a shared mutex

--- core/pool/include/Pool.h
+++ core/pool/include/Pool.h
@@ -2,6 +2,7 @@
 #define LIGHTDB_POOL_H
 
 #include "MaterializedLightField.h"
+#include <shared_mutex>
 
 namespace lightdb {
 
@@ -22,15 +23,20 @@ namespace lightdb {
             }
 
             static shared_reference<T, BufferPoolEntry> get(const T& instance) {
+                std::shared_lock lock(mutex_);
                 auto weak = lookup_.at(&instance);
                 auto shared = weak.lock();
                 return shared_reference<T, BufferPoolEntry>(shared);
             };
 
-            static size_t size() { return lookup_.size(); }
+            static size_t size() {
+                std::shared_lock lock(mutex_);
+                return lookup_.size();
+            }
 
         private:
             static void Register(const std::shared_ptr<T>& pointer) {
+                std::unique_lock lock(mutex_);
                 assert(pointer != nullptr);
                 lookup_.emplace(std::make_pair(&*pointer, std::weak_ptr<T>(pointer)));
             }
@@ -40,15 +46,19 @@ namespace lightdb {
             }
 
             static void Unregister(const T* pointer) {
+                std::unique_lock lock(mutex_);
                 if(pointer != nullptr && lookup_.at(pointer).use_count() == 0)
                     lookup_.erase(pointer);
             }
 
             static std::unordered_map<const T*, std::weak_ptr<T>> lookup_;
+            static std::shared_mutex mutex_;
         };
 
         template<typename T>
         std::unordered_map<const T*, std::weak_ptr<T>> BufferPoolEntry<T>::lookup_{};
+        template<typename T>
+        std::shared_mutex BufferPoolEntry<T>::mutex_{};
     } // namespace pool
 } // namespace lightdb
 
