78a4fed519df8286519379bafff8954965c9dfc8
Brandon Haynes <bhaynes@cs.washington.edu>
2020-02-07 13:20:36 -0800

    CUVID init before CUDA

--- core/video/src/GPUContext.cc
+++ core/video/src/GPUContext.cc
@@ -9,10 +9,10 @@ bool GPUContext::Initialize() {
 
     if(initialized_)
         return true;
-    else if((result = cuInit(0)) != CUDA_SUCCESS)
-        throw GpuCudaRuntimeError("Call to cuInit failed", result);
     else if((result = cuvidInit(0)) != CUDA_SUCCESS)
         throw GpuCudaRuntimeError("Call to cuvidInit failed", result);
+    else if((result = cuInit(0)) != CUDA_SUCCESS)
+        throw GpuCudaRuntimeError("Call to cuInit failed", result);
     else
         return (initialized_ = true);
 }
